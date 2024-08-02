c2eaaf43a79e5e4fc5c8b12a7574b78cc074bd83
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-08-02 21:02:24 +0500

    changed pdf send logic

--- routes/courseRoute/servePDF.route.js
+++ routes/courseRoute/servePDF.route.js
@@ -1,19 +1,41 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const fs = require('fs')
+
 
 router.get('/:year/:subject/:type/:filename', (req, res) => {
     console.log("Here",)
     const { year, subject, type, filename } = req.params;
     const filePath = path.join(__dirname, "..", "..", 'pastpapers', year, subject, type, filename);
 
-
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            console.log('Error serving the file:', err);
-            res.status(404).send('File not found');
+    fs.access(filePath, fs.constants.F_OK,
+        (err) => {
+            if (err) {
+                console.log('Error: File not found', err);
+                res.status(404).send('File not found');
+                return;
+            }
         }
-    });
+    )
+
+    const fileStream = fs.createReadStream(filePath)
+
+    fileStream.on('error', (streamErr) => {
+        console.log('Error streaming the file:', streamErr);
+        res.status(500).send('Error reading the file');
+    })
+
+    res.setHeader('Content-Disposition', `inline; filename="${filename}"`);
+    res.setHeader('Content-Type', 'application/pdf');
+    fileStream.pipe(res);
+
+    // res.sendFile(filePath, (err) => {
+    //     if (err) {
+    //         console.log('Error serving the file:', err);
+    //         res.status(404).send('File not found');
+    //     }
+    // });
 });
 
 module.exports = router
