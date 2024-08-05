65f82d43b523da1f2a17e6dd1590255e3a2a2ddb
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-08-05 05:29:25 +0500

    Configure redis io client

--- db/redisClient.js
+++ db/redisClient.js
@@ -1,13 +1,29 @@
-const redis = require('redis');
 
-const client = redis.createClient({
-    url: 'redis://localhost:6379'
+const Redis = require('ioredis');
+
+const redis = new Redis({
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD,
+    db: 0
 });
 
-client.on('error', (err) => console.error('Redis Client Error', err));
-const startRedis = async () => {
-    await client.connect();
-}
-startRedis()
+redis.on('error', (err) => {
+    console.error('Redis error:', err);
+});
 
-module.exports = client;
+module.exports = redis;
+
+// const redis = require('redis');
+
+// const client = redis.createClient({
+//     url: 'redis://localhost:6379'
+// });
+
+// client.on('error', (err) => console.error('Redis Client Error', err));
+// const startRedis = async () => {
+//     await client.connect();
+// }
+// startRedis()
+
+// module.exports = client;
