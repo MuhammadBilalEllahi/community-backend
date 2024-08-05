e353c8afe778bba7d1cb122a8becc760ccbd418e
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-08-05 05:46:41 +0500

    redis private url added

--- db/redisClient.js
+++ db/redisClient.js
@@ -1,29 +1,29 @@
 
-const Redis = require('ioredis');
+// const Redis = require('ioredis');
 
-const redis = new Redis({
-    port: process.env.REDIS_PORT,
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD,
-    db: 0
-});
-
-redis.on('error', (err) => {
-    console.error('Redis error:', err);
-});
-
-module.exports = redis;
-
-// const redis = require('redis');
-
-// const client = redis.createClient({
-//     url: 'redis://localhost:6379'
+// const redis = new Redis({
+//     port: process.env.REDIS_PORT,
+//     host: process.env.REDIS_HOST,
+//     password: process.env.REDIS_PASSWORD,
+//     db: 0
 // });
 
-// client.on('error', (err) => console.error('Redis Client Error', err));
-// const startRedis = async () => {
-//     await client.connect();
-// }
-// startRedis()
+// redis.on('error', (err) => {
+//     console.error('Redis error:', err);
+// });
+
+// module.exports = redis;
+
+const redis = require('redis');
+
+const client = redis.createClient({
+    url: process.env.REDIS_URL
+});
+
+client.on('error', (err) => console.error('Redis Client Error', err));
+const startRedis = async () => {
+    await client.connect();
+}
+startRedis()
 
 // module.exports = client;
