17f9aa2c5c95214328f9b2eb8602f5f63ebf7560
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-08-10 02:35:12 +0500

    env var change

--- db/reddis.js
+++ db/reddis.js
@@ -1,9 +1,9 @@
 const Redis = require('ioredis');
 
 const redis = new Redis({
-    port: process.env.REDIS_PORT,
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD,
+    port: process.env.REDISPORT,
+    host: process.env.REDISHOST,
+    password: process.env.REDISPASSWORD,
     db: 0
 });
 
